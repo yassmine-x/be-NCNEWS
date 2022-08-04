05956c3a1281102efc43454e1da2f83448d1e312
yassmine-x <106983303+yassmine-x@users.noreply.github.com>
2022-08-04 12:28:53 +0100

    Update all_articles.model.js

--- models/all_articles.model.js
+++ models/all_articles.model.js
@@ -5,11 +5,9 @@ const fetchArticles = () => {
       "SELECT articles.*, COUNT(comments.article_id) ::INT AS comment_count FROM articles LEFT JOIN comments ON comments.article_id=articles.article_id  LEFT JOIN users ON articles.author=users.username GROUP BY articles.article_id ORDER BY articles.created_at DESC;"
     )
     .then(({ rows }) => {
-      console.log(rows);
       return rows;
     });
 };
 
 module.exports = fetchArticles;
 
-//  "SELECT articles.*, COUNT(comments.article_id) ::INT AS comment_count FROM comments RIGHT JOIN articles ON articles.article_id=comments.article_id GROUP BY articles.article_id; SELECT authors FROM articles LEFT JOIN users ON articles.authors=users.username"
