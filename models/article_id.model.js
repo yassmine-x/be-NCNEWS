b4e7d665b9d268e8244df3cdbabbd2359d079b01
Yassmine <ccuk@ikos-MacBook-Pro.local>
2022-08-04 10:28:00 +0100

    ticket7 complete

--- models/article_id.model.js
+++ models/article_id.model.js
@@ -2,7 +2,10 @@ const db = require("../db/connection");
 
 const fetchArticle = (id) => {
   return db
-    .query("SELECT * FROM articles WHERE article_id = $1", [id])
+    .query(
+      "SELECT articles.*, COUNT(comments.article_id) ::INT AS comment_count FROM comments RIGHT JOIN articles ON articles.article_id=comments.article_id GROUP BY articles.article_id HAVING articles.article_id = $1",
+      [id]
+    )
     .then(({ rows: article }) => {
       if (article.length === 0) {
         return Promise.reject({ status: 404, msg: "Article not found" });
