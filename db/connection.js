0de5018693816dfd094fff49afc0e4eed4dcc6c8
Yassmine <ccuk@ikos-MacBook-Pro.local>
2023-01-03 16:03:58 +0000

    first few steps of hosting

--- db/connection.js
+++ db/connection.js
@@ -1,18 +1,14 @@
 const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
-
 require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
-
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
-
 // if (!process.env.PGDATABASE) {
 //   throw new Error("PGDATABASE not set");
 // }
-
 // const config = {};
 // if (ENV === "production") {
 //   config.connectionString = process.env.DATABASE_URL;
@@ -20,16 +16,12 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
 //     rejectUnauthorized: false,
 //   };
 // }
-
 // module.exports = new Pool(config);
-
 const config =
   ENV === "production"
     ? {
         connectionString: process.env.DATABASE_URL,
-        ssl: {
-          rejectUnauthorized: false,
-        },
+        max: 2,
       }
     : {};
 
--- listen.js
+++ listen.js
@@ -2,7 +2,9 @@ const app = require("./app");
 
 const { PORT = 9090 } = process.env;
 
-app.listen(PORT, (err) => {
-  if (err) throw err;
-  console.log(`Listening on ${PORT}...`);
-});
+// app.listen(PORT, (err) => {
+//   if (err) throw err;
+//   console.log(`Listening on ${PORT}...`);
+// });
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
